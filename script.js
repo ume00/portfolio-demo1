9838590619435617f2398df158ecc64c224e3073
umeme00 <ufme.0723@gmail.com>
2024-06-30 15:12:36 +0700

    update：画面幅378px以下のレイアウト修正

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ $('.ms-container').multiscroll({
     navigation: true,
     navigationPosition: 'right',
 });
+
+$(function() {
+    var $viewport = $('meta[name="viewport"]');
+    
+    function switchViewport() {
+      var value = $(window).outerWidth() > 360
+        ? 'width=device-width,initial-scale=1'
+        : 'width=360';
+      
+      if ($viewport.attr('content') !== value) {
+        $viewport.attr('content', value);
+      }
+    }
+    
+    $(window).on('resize', switchViewport);
+    switchViewport();
+  });
