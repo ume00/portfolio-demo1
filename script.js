80d119be6c11dd0b7fafbf0fbc08a4383906c6b7
ume00 <freeworker.life@gmail.com>
2024-07-02 17:34:12 +0700

    add：trial_nav作成

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-if ($('body').hasClass('top')) {
-  $('.ms-container').multiscroll({
-    anchors: ['top', '', '','','',''],
-	  loopTop: false,
-	  loopBottom: true,
-    navigation: true,
-    navigationPosition: 'right',
-  });
-}
-
+/* top_multiscroll */
 $(function() {
-    var $viewport = $('meta[name="viewport"]');
-    
-    function switchViewport() {
-      var value = $(window).outerWidth() > 360
-        ? 'width=device-width,initial-scale=1'
-        : 'width=360';
+  if ($('body').hasClass('top')) {
+    $('.ms-container').multiscroll({
+      anchors: ['top', '', '','','',''],
+	    loopTop: false,
+	    loopBottom: true,
+      navigation: true,
+      navigationPosition: 'right',
+    });
+  }
+
+  /* common_viewport */
+  var $viewport = $('meta[name="viewport"]');
+  function switchViewport() {
+    var value = $(window).outerWidth() > 360
+    ? 'width=device-width,initial-scale=1'
+    : 'width=360';
       
-      if ($viewport.attr('content') !== value) {
-        $viewport.attr('content', value);
-      }
+    if ($viewport.attr('content') !== value) {
+      $viewport.attr('content', value);
     }
+  }
     
-    $(window).on('resize', switchViewport);
-    switchViewport();
+  $(window).on('resize', switchViewport);
+  switchViewport();
+
+  /* page-common_current */
+  $(".nav-item a").each(function(){
+    if(this.href == location.href) {
+      $(this).parents(".nav-item").addClass("current");
+    }
   });
+});
